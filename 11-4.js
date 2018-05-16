3b818703fd9322d9508b8eb039fe37091f5d2aa8
Monika Magda <monikawmagda@gmail.com>
2018-05-16 19:16:13 +0200

    modify 4 and add 5

--- 11-4.js
+++ 11-4.js
@@ -6,15 +6,19 @@ function Phone(brand, price, color) {
 	this.color = color;
 }
 
-Phone.prototype.printInfo = function () {
-    console.log("The phone brand is " + this.brand + " , color is " + this.color + " and the price is " + this.price + ".");
-};
-
 var iphone6s = new Phone("Apple", 2250, "silver");
 var SamsungGalaxyS6 = new Phone("Samsung", 1000, "grey");
 var OnePlusOne = new Phone("NieWiem", 1500, "white");
 
+Phone.prototype.printInfo = function () {
+console.log("The phone brand is " + this.brand + " , color is " + this.color + " and the price is " + this.price + ".");
+};
+
+
 iphone6s.printInfo();
+SamsungGalaxyS6.printInfo();
+OnePlusOne.printInfo();
+
 
 
 
